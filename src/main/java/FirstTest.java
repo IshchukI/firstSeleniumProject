80624387dd8618b58bda8d68cae2fb98546abe35
Iaroslav Ishchuk <yaroslavischuck@gmail.com>
2021-11-01 18:11:49 +0200

    error test completed

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -27,48 +27,74 @@ public class FirstTest {
          *  Эта часть для заполнения данных, здесь нет проверок
          *
          * */
+        //------------кредлы
+        String wrongEmail = "somegmail@gmail.com";
+        String wrongPass = "1122334455";
 
         //------------ Находим элемент и создаем переменную
+        By userName = By.xpath(".//input[@name='email']");
+        By userPassword = By.xpath(".//input[@name='pass']");
+        By submitButton = By.xpath(".//button[@name='login']");
+        By textErrorLogin = By.xpath(".//a[@href=\"https://www.facebook.com/recover/initiate?lwv=120&lwc=1348092&ars=facebook_login_pw_error\"]");
 
-        By commentText = By.xpath(".//div[@data-qa-node='comment']");
-        By receiverAmount = By.xpath(".//div[@data-qa-node='receiver-amount']");
-        By payerCard = By.xpath(".//span[@data-qa-node='payer-card']");
-        By receiverCard = By.xpath(".//span[@data-qa-node='receiver-card']");
-        By cardNumberFrom = By.xpath(".//input[@data-qa-node='numberdebitSource']");
 
         //------------ Этот блок с операциями над элементами
 
-        driver.get("https://next.privat24.ua/money-transfer/card");
-        // здесь создали переменную cardNumberFrom и присовили ей элемент по адрессу в DOM модели с помощью XPATH
-        driver.findElement(cardNumberFrom).sendKeys("4552331448138217");
-        //в примере ниже мы не создаем переменную, а сразу работаем с элементом по его XPATH локатору
-        driver.findElement(By.xpath(".//input[@data-qa-node='expiredebitSource']")).sendKeys("0524");
-        driver.findElement(By.xpath(".//input[@data-qa-node='cvvdebitSource']")).sendKeys("111");
-        driver.findElement(By.xpath(".//input[@data-qa-node='firstNamedebitSource']")).sendKeys("Shayne");
-        driver.findElement(By.xpath(".//input[@data-qa-node='lastNamedebitSource']")).sendKeys("McConnell");
-        driver.findElement(By.xpath(".//input[@data-qa-node='numberreceiver']")).sendKeys("4567739561253907");
-        driver.findElement(By.xpath(".//input[@data-qa-node='firstNamereceiver']")).sendKeys("Ruth");
-        driver.findElement(By.xpath(".//input[@data-qa-node='lastNamereceiver']")).sendKeys("Montalvo");
-        driver.findElement(By.xpath(".//input[@data-qa-node='amount']")).clear();
-        driver.findElement(By.xpath(".//input[@data-qa-node='amount']")).sendKeys("300");
-        driver.findElement(By.xpath(".//span[@data-qa-node='toggle-comment']")).click();
-        driver.findElement(By.xpath(".//textarea[@data-qa-node='comment']")).sendKeys("На текилу");
-        driver.findElement(By.xpath(".//button[contains(text(), 'Переказати')]")).click();
-
+        driver.get("https://www.facebook.com/");
+        driver.findElement(userName).sendKeys(wrongEmail);
+        driver.findElement(userPassword).sendKeys(wrongPass);
+        driver.findElement(submitButton).click();
         /**
          *
          *  Эта часть для создания проверок в тесте
          *
          * */
 
-        Assert.assertEquals("На текилу", driver.findElement(commentText).getText());
-        Assert.assertEquals("300 UAH", driver.findElement(receiverAmount).getText());
-        Assert.assertEquals("* 8217", driver.findElement(payerCard).getText());
-        Assert.assertEquals("* 3907", driver.findElement(receiverCard).getText());
+        Assert.assertEquals("Забыли пароль?", driver.findElement(textErrorLogin).getText());
 
-        // -------------- После проверки экрана прогноза платежа, переходим к оплате в Корзину
 
-        driver.findElement(By.xpath(".//button[contains(text(), 'В кошик')]")).click();
+
+//        //------------ Находим элемент и создаем переменную
+//
+//        By commentText = By.xpath(".//div[@data-qa-node='comment']");
+//        By receiverAmount = By.xpath(".//div[@data-qa-node='receiver-amount']");
+//        By payerCard = By.xpath(".//span[@data-qa-node='payer-card']");
+//        By receiverCard = By.xpath(".//span[@data-qa-node='receiver-card']");
+//        By cardNumberFrom = By.xpath(".//input[@data-qa-node='numberdebitSource']");
+//
+//        //------------ Этот блок с операциями над элементами
+//
+//        driver.get("https://next.privat24.ua/money-transfer/card");
+//        // здесь создали переменную cardNumberFrom и присовили ей элемент по адрессу в DOM модели с помощью XPATH
+//        driver.findElement(cardNumberFrom).sendKeys("4552331448138217");
+//        //в примере ниже мы не создаем переменную, а сразу работаем с элементом по его XPATH локатору
+//        driver.findElement(By.xpath(".//input[@data-qa-node='expiredebitSource']")).sendKeys("0524");
+//        driver.findElement(By.xpath(".//input[@data-qa-node='cvvdebitSource']")).sendKeys("111");
+//        driver.findElement(By.xpath(".//input[@data-qa-node='firstNamedebitSource']")).sendKeys("Shayne");
+//        driver.findElement(By.xpath(".//input[@data-qa-node='lastNamedebitSource']")).sendKeys("McConnell");
+//        driver.findElement(By.xpath(".//input[@data-qa-node='numberreceiver']")).sendKeys("4567739561253907");
+//        driver.findElement(By.xpath(".//input[@data-qa-node='firstNamereceiver']")).sendKeys("Ruth");
+//        driver.findElement(By.xpath(".//input[@data-qa-node='lastNamereceiver']")).sendKeys("Montalvo");
+//        driver.findElement(By.xpath(".//input[@data-qa-node='amount']")).clear();
+//        driver.findElement(By.xpath(".//input[@data-qa-node='amount']")).sendKeys("300");
+//        driver.findElement(By.xpath(".//span[@data-qa-node='toggle-comment']")).click();
+//        driver.findElement(By.xpath(".//textarea[@data-qa-node='comment']")).sendKeys("На текилу");
+//        driver.findElement(By.xpath(".//button[contains(text(), 'Переказати')]")).click();
+//
+//        /**
+//         *
+//         *  Эта часть для создания проверок в тесте
+//         *
+//         * */
+//
+//        Assert.assertEquals("На текилу", driver.findElement(commentText).getText());
+//        Assert.assertEquals("300 UAH", driver.findElement(receiverAmount).getText());
+//        Assert.assertEquals("* 8217", driver.findElement(payerCard).getText());
+//        Assert.assertEquals("* 3907", driver.findElement(receiverCard).getText());
+//
+//        // -------------- После проверки экрана прогноза платежа, переходим к оплате в Корзину
+//
+//        driver.findElement(By.xpath(".//button[contains(text(), 'В кошик')]")).click();
 
     }
 }
