937c9740c25fdcd61fcead541d61cb927a874c90
Iaroslav Ishchuk <yaroslavischuck@gmail.com>
2021-11-01 19:23:14 +0200

    finish

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -40,6 +41,7 @@ public class FirstTest {
 
         //------------ Этот блок с операциями над элементами
         driver.get("https://www.facebook.com/");
+        driver.manage().window().maximize();
         driver.findElement(userName).sendKeys(wrongEmail);
         driver.findElement(userPassword).sendKeys(wrongPass);
         driver.findElement(submitButton).click();
@@ -51,7 +53,7 @@ public class FirstTest {
         Assert.assertEquals("Забыли пароль?", driver.findElement(textErrorLogin).getText());
 
         //------------Выходим из браузера
-        driver.quit();
+        //driver.quit();
     }
 
     @Test
@@ -63,9 +65,10 @@ public class FirstTest {
          *  так же, здесь указали точно, где взять движок через setProperty
          *  так же здесь указали, что нужно ожидать элемент на UI в течение 3 сек прежде, чем с ним взаимодействовать
          * */
-
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
         System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
         /**
@@ -86,6 +89,7 @@ public class FirstTest {
 
         //------------ Этот блок с операциями над элементами
         driver.get("https://www.facebook.com/");
+        driver.manage().window().maximize();
         driver.findElement(userName).sendKeys(Email);
         driver.findElement(userPassword).sendKeys(Pass);
         driver.findElement(submitButton).click();
@@ -97,50 +101,7 @@ public class FirstTest {
         Assert.assertEquals("Ярослав Ищук", driver.findElement(nameAfterLogin).getText());
 
         //------------Выходим из браузера
-        driver.quit();
+        //driver.quit();
 
     }
 }
-
-
-//        //------------ Находим элемент и создаем переменную
-//
-//        By commentText = By.xpath(".//div[@data-qa-node='comment']");
-//        By receiverAmount = By.xpath(".//div[@data-qa-node='receiver-amount']");
-//        By payerCard = By.xpath(".//span[@data-qa-node='payer-card']");
-//        By receiverCard = By.xpath(".//span[@data-qa-node='receiver-card']");
-//        By cardNumberFrom = By.xpath(".//input[@data-qa-node='numberdebitSource']");
-//
-//        //------------ Этот блок с операциями над элементами
-//
-//        driver.get("https://next.privat24.ua/money-transfer/card");
-//        // здесь создали переменную cardNumberFrom и присовили ей элемент по адрессу в DOM модели с помощью XPATH
-//        driver.findElement(cardNumberFrom).sendKeys("4552331448138217");
-//        //в примере ниже мы не создаем переменную, а сразу работаем с элементом по его XPATH локатору
-//        driver.findElement(By.xpath(".//input[@data-qa-node='expiredebitSource']")).sendKeys("0524");
-//        driver.findElement(By.xpath(".//input[@data-qa-node='cvvdebitSource']")).sendKeys("111");
-//        driver.findElement(By.xpath(".//input[@data-qa-node='firstNamedebitSource']")).sendKeys("Shayne");
-//        driver.findElement(By.xpath(".//input[@data-qa-node='lastNamedebitSource']")).sendKeys("McConnell");
-//        driver.findElement(By.xpath(".//input[@data-qa-node='numberreceiver']")).sendKeys("4567739561253907");
-//        driver.findElement(By.xpath(".//input[@data-qa-node='firstNamereceiver']")).sendKeys("Ruth");
-//        driver.findElement(By.xpath(".//input[@data-qa-node='lastNamereceiver']")).sendKeys("Montalvo");
-//        driver.findElement(By.xpath(".//input[@data-qa-node='amount']")).clear();
-//        driver.findElement(By.xpath(".//input[@data-qa-node='amount']")).sendKeys("300");
-//        driver.findElement(By.xpath(".//span[@data-qa-node='toggle-comment']")).click();
-//        driver.findElement(By.xpath(".//textarea[@data-qa-node='comment']")).sendKeys("На текилу");
-//        driver.findElement(By.xpath(".//button[contains(text(), 'Переказати')]")).click();
-//
-//        /**
-//         *
-//         *  Эта часть для создания проверок в тесте
-//         *
-//         * */
-//
-//        Assert.assertEquals("На текилу", driver.findElement(commentText).getText());
-//        Assert.assertEquals("300 UAH", driver.findElement(receiverAmount).getText());
-//        Assert.assertEquals("* 8217", driver.findElement(payerCard).getText());
-//        Assert.assertEquals("* 3907", driver.findElement(receiverCard).getText());
-//
-//        // -------------- После проверки экрана прогноза платежа, переходим к оплате в Корзину
-//
-//        driver.findElement(By.xpath(".//button[contains(text(), 'В кошик')]")).click();
