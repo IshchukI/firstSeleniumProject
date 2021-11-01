2f3a9ecad2bf5c8432fbfc52aaaceb946c8b8c45
Iaroslav Ishchuk <yaroslavischuck@gmail.com>
2021-11-01 18:50:44 +0200

    login test completed

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 public class FirstTest {
 
     @Test
-    public void checkUrl() {
+    public void checkLoginWithError() {
 
         /**
          *
@@ -18,7 +18,7 @@ public class FirstTest {
          *  так же здесь указали, что нужно ожидать элемент на UI в течение 3 сек прежде, чем с ним взаимодействовать
          * */
 
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
@@ -28,18 +28,17 @@ public class FirstTest {
          *
          * */
         //------------кредлы
-        String wrongEmail = "somegmail@gmail.com";
+        String wrongEmail = "yaroslavischuck@gmail.com";
         String wrongPass = "1122334455";
 
         //------------ Находим элемент и создаем переменную
         By userName = By.xpath(".//input[@name='email']");
         By userPassword = By.xpath(".//input[@name='pass']");
         By submitButton = By.xpath(".//button[@name='login']");
-        By textErrorLogin = By.xpath(".//a[@href=\"https://www.facebook.com/recover/initiate?lwv=120&lwc=1348092&ars=facebook_login_pw_error\"]");
+        By textErrorLogin = By.xpath(".//a[@href=\'https://www.facebook.com/recover/initiate?lwv=120&lwc=1348092&ars=facebook_login_pw_error\']");
 
 
         //------------ Этот блок с операциями над элементами
-
         driver.get("https://www.facebook.com/");
         driver.findElement(userName).sendKeys(wrongEmail);
         driver.findElement(userPassword).sendKeys(wrongPass);
@@ -49,9 +48,59 @@ public class FirstTest {
          *  Эта часть для создания проверок в тесте
          *
          * */
-
         Assert.assertEquals("Забыли пароль?", driver.findElement(textErrorLogin).getText());
 
+        //------------Выходим из браузера
+        driver.quit();
+    }
+
+    @Test
+    public void checkLogin () {
+
+        /**
+         *
+         *  Эта часть для создания драйвера - образа движка определенного браузера
+         *  так же, здесь указали точно, где взять движок через setProperty
+         *  так же здесь указали, что нужно ожидать элемент на UI в течение 3 сек прежде, чем с ним взаимодействовать
+         * */
+
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        /**
+         *
+         *  Эта часть для заполнения данных, здесь нет проверок
+         *
+         * */
+        //------------кредлы
+        String Email = "iaroslavishchuk@gmail.com";
+        String Pass = "123qweASD";
+
+        //------------ Находим элемент и создаем переменную
+        By userName = By.xpath(".//input[@name='email']");
+        By userPassword = By.xpath(".//input[@name='pass']");
+        By submitButton = By.xpath(".//button[@name='login']");
+        By nameAfterLogin = By.xpath(".//span[contains(text(), 'Ярослав Ищук')]");
+
+
+        //------------ Этот блок с операциями над элементами
+        driver.get("https://www.facebook.com/");
+        driver.findElement(userName).sendKeys(Email);
+        driver.findElement(userPassword).sendKeys(Pass);
+        driver.findElement(submitButton).click();
+        /**
+         *
+         *  Эта часть для создания проверок в тесте
+         *
+         * */
+        Assert.assertEquals("Ярослав Ищук", driver.findElement(nameAfterLogin).getText());
+
+        //------------Выходим из браузера
+        driver.quit();
+
+    }
+}
 
 
 //        //------------ Находим элемент и создаем переменную
@@ -95,6 +144,3 @@ public class FirstTest {
 //        // -------------- После проверки экрана прогноза платежа, переходим к оплате в Корзину
 //
 //        driver.findElement(By.xpath(".//button[contains(text(), 'В кошик')]")).click();
-
-    }
-}
